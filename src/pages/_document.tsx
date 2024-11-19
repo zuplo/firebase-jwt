24f01d515a69055c3984ef26735d24ef09fd5172
Josh Twist <josh@molmorg.com>
2024-11-18 21:10:50 -0800

    added GTM

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* GTM Script */}
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M7H4Z5BW');`,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-
-      <script
-        src="https://cdn.usefathom.com/script.js"
-        data-site="YEMJHIZR"
-        defer
-      ></script>
     </Html>
   );
 }
